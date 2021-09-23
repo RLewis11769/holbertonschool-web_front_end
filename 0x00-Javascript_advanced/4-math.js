58eeaf72dd3e84e461ff37bcf77e33de1665f676
Rachel Lewis <RLewis11769@gmail.com>
2021-09-23 02:27:49 +0000

    Add data to 4-math

---
+++ 0x00-Javascript_advanced/4-math.js
@@ -0,0 +1,14 @@
+/* Closure = function with access to parent scope even after parent has closed */
+
+function divideBy (firstNumber) {
+  return (secondNumber) => secondNumber / firstNumber;
+}
+
+function addBy (firstNumber) {
+  return (secondNumber) => firstNumber + secondNumber;
+}
+
+let addBy100 = addBy(100);
+let addBy1000 = addBy(1000);
+let divideBy10 = divideBy(10);
+let divideBy100 = divideBy(100);
